f8b3cb303fba5b132e23bc30e2a752c9559b6949
kamiga110 <145092514+kamiga110@users.noreply.github.com>
2023-11-22 16:33:38 +0900

    Jquery+

--- myprofile/script.js
+++ myprofile/script.js
@@ -25,4 +25,43 @@ $(window).scroll(()=>{
     $('#pagetop').fadeOut();
   }
 });
+
+$('a[href^="#"]').click(function () {
+    const speed = 500;
+    const href = $('a[href^="#"]').attr('href');
+    let $target;
+    if (href == '#') {
+      $target = $('html');
+    }
+    else {
+      $target = $(href);
+    }
+    const position = $target.offset().top;
+    $('html, body').animate({ 'scrollTop': position }, speed, 'swing');
+    return false;
+  });
+
+  $(window).scroll(function(){
+    $('section').each(function (){
+      const pos = $(this).offset().top;
+      const scroll = $(window).scrollTop();
+      const windowHeight = $(window).height();
+      if (scroll > pos - windowHeight + 100){
+        $(this).addClass('fadein');
+      }
+    });
+  });
+
+  $('.works-pic img').click(function(){
+    const imgSrc = $(this).attr('src');
+    $('.big-img').attr('src',imgSrc);
+    $('.modal').fadeIn();
+    return false
+  });
+
+  // ×を押したら閉じる
+  $('.close-btn').click(function(){
+    $('.modal').fadeOut();
+    return false
+  });
 });
