b3a84f6437a0ef76b71a863f2bc07bea8c5a01e2
kamiga110 <145092514+kamiga110@users.noreply.github.com>
2024-01-01 00:18:15 +0900

    12/31更新

--- tsuchiya-seiko/script.js
+++ tsuchiya-seiko/script.js
@@ -1,5 +1,14 @@
 
-
+$(function(){
+// ハンバーガーメニュー
 $(".openbtn1").click(function () {
   $(this).toggleClass('active');
+});
+
+// ハンバーガー開閉メニュー
+$(".openbtn1").click(function(){
+  $('.hamburger').toggleClass('hamu-open');
+});
+
+  
 });
